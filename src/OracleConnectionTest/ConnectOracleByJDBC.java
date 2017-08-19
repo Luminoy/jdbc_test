48ddefea1a13feaa953617117c97cdf87f6a0b3e
tplumink <lumin@tplink.com.cn>
2017-08-19 16:01:37 +0800

    利用事务完成增删改操作。

--- src/OracleConnectionTest/ConnectOracleByJDBC.java
+++ src/OracleConnectionTest/ConnectOracleByJDBC.java
@@ -1,39 +1,48 @@
 package OracleConnectionTest;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.LinkedList;
+import java.util.List;
 
 public class ConnectOracleByJDBC {
 
+	public static List getData() {
+		List dataList = null;
+
+		try {
+			dataList = new LinkedList<Object[]>();
+			DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+			dataList.add(new Object[] { 450001, "Kobe.Bryant", "male",
+					sdf.parse("1977-08-24") });
+			dataList.add(new Object[] { 450002, "Michael.Jordan", "male",
+					sdf.parse("1962-04-23") });
+			dataList.add(new Object[] { 450005, "James.Laurance", "female",
+					sdf.parse("1987-11-12") });
+			dataList.add(new Object[] { 450006, "JR.Smith", "male",
+					sdf.parse("1985-07-07") });
+		} catch (ParseException e) {
+			System.out.println("日期解析出错！");
+		}
+
+		return dataList;
+	}
+
 	public static void main(String[] args) {
 		Connection conn = JDBCUtils.getOracleConnection();
-		String tbName = "xxtp_lumin_student_tl";
-		JDBCUtils.createOracleTable(conn, tbName);
-		String sql = "select * from " + tbName;
-		ResultSet resultSet = JDBCUtils.getOracleQueryResult(conn, sql);
-		String dno = null;
-		String dname = null;
-		System.out.println("顺序查：");
-		try {
-			while (resultSet.next()) {
-				dno = resultSet.getString(1);
-				dname = resultSet.getString(2);
-				System.out.println(dno + ": " + dname);
-			}
-		
-		System.out.println("逆序查：");
-		while(resultSet.previous()) {
-			dno = resultSet.getString(1);
-			dname = resultSet.getString(2);
-			System.out.println(dno + ": " + dname);
-			}
+		String tName = "XXTP_LUMIN_STUDENT_TL1";
+		JDBCUtils.createOracleTable(conn, tName);
 
-		conn.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} 
+		List dataList = getData();
+		JDBCUtils.insertDataIntoOracleTable(conn, dataList, tName);
+		ResultSet resultSet = JDBCUtils.getOracleQueryResult(conn, tName);
+		JDBCUtils.printResultSet(resultSet);
+
+		JDBCUtils.deleteDataFromOracleTable(conn, tName);
+		JDBCUtils.dropOracleTable(conn, tName);
+		JDBCUtils.closeConnection(conn);
 	}
 }
--- src/OracleConnectionTest/JDBCUtils.java
+++ src/OracleConnectionTest/JDBCUtils.java
@@ -5,111 +5,213 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Savepoint;
 import java.sql.Statement;
 import java.util.List;
 
 public class JDBCUtils {
+	private static String dbUrl = "jdbc:oracle:thin:@172.29.41.138:1521:DEMO";
+	private static String dbUserName = "scott";
+	private static String dbPassword = "tiger";
+
+	// 获取数据库连接
 	public static Connection getOracleConnection() {
 		Connection connection = null;
 
-		String url = "jdbc:oracle:thin:@172.29.41.138:1521:DEMO";
-		String uname = "scott";
-		String upwd = "tiger";
 		try {
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			connection = DriverManager.getConnection(url, uname, upwd);
+			connection = DriverManager.getConnection(dbUrl, dbUserName,
+					dbPassword);
 
 		} catch (ClassNotFoundException e) {
-			System.out.println("driver not found!");
-			// e.printStackTrace();
+			System.out.println("数据库类名查找失败！");
 		} catch (SQLException e) {
-			System.out.println("get connection error!");
-			// e.printStackTrace();
+			System.out.println("数据库连接参数错误！");
 		}
 		if (connection == null) {
-			System.out.println("connection failed!");
+			System.out.println("数据库连接失败！");
 			return null;
 		} else {
-			System.out.println("connection succeed!");
+			System.out.println("数据库连接成功！");
 			return connection;
 		}
 	}
 
-	public static ResultSet getOracleQueryResult(Connection conn, String sql) {
+	// 建立student表
+	public static boolean createOracleTable(Connection conn, String tName) {
+		Statement statement = null;
+		try {
+			statement = conn.createStatement();
+			statement.executeUpdate("create table " + tName + "("
+					+ "id number(10) primary key not null,"
+					+ "name varchar2(50)," + "sex varchar2(10),"
+					+ "birthday date" + ")");
+		} catch (SQLException e) {
+			System.out.println("创建表出错！");
+			return false;
+		}
+		System.out.println("创建表成功！");
+		return true;
+	}
+
+	// 插入若干行数据
+	public static boolean insertDataIntoOracleTable(Connection conn,
+			List dataList, String tName) {
+		PreparedStatement preStatement = null;
+		Savepoint sp = null;
+		try {
+			if (dataList == null) {
+				System.out.println("插入数据源为空！");
+				return false;
+			}
+			// 关闭自动提交事务设置
+			conn.setAutoCommit(false);
+			// 设置一个存储点
+			sp = conn.setSavepoint();
+
+			preStatement = conn.prepareStatement("insert into " + tName
+					+ "(id, name, sex, birthday) Values(?,?,?,?)");
+
+			// 设置插入数据
+			for (int i = 0; i < dataList.size(); i++) {
+				Object[] objs = (Object[]) dataList.get(i);
+				for (int j = 0; j < objs.length; j++) {
+					preStatement.setObject(j + 1, objs[j]);
+				}
+				preStatement.executeUpdate();
+			}
+
+			// 手动提交事务
+			conn.commit();
+			// 恢复自动提交事务设置
+			conn.setAutoCommit(true);
+		} catch (Exception e) {
+			System.out.println(tName + "表数据插入过程中出错！");
+			System.out.println("出错信息：" + e.getMessage());
+			try {
+				System.out.println("事务回滚至存储点...");
+				// 回滚
+				conn.rollback(sp);
+				preStatement.close();
+				conn.setAutoCommit(true);
+				System.out.println("回滚成功！");
+			} catch (SQLException e1) {
+				System.out.println("回滚失败！");
+			}
+			return false;
+		}
+		return true;
+	}
+
+	// 删除所有数据
+	public static boolean deleteDataFromOracleTable(Connection conn,
+			String tName) {
+		PreparedStatement preStatement = null;
+		Savepoint sp = null;
+		try {
+			// 关闭自动提交事务设置
+			conn.setAutoCommit(false);
+			// 设置一个存储点
+			sp = conn.setSavepoint();
+
+			preStatement = conn.prepareStatement("delete from " + tName);
+			preStatement.executeUpdate();
+
+			// 手动提交事务
+			conn.commit();
+			// 恢复自动提交事务设置
+			conn.setAutoCommit(true);
+		} catch (SQLException e) {
+			System.out.println(tName + "表数据删除过程中出错！");
+			System.out.println("出错信息：" + e.getLocalizedMessage());
+			try {
+				System.out.println("事务回滚至存储点...");
+				// 回滚
+				conn.rollback(sp);
+				preStatement.close();
+				conn.setAutoCommit(true);
+				System.out.println("回滚成功！");
+			} catch (SQLException e1) {
+				System.out.println("回滚失败！");
+			}
+			return false;
+		}
+		return true;
+	}
+
+	// 查询数据，获得可滚动的结果集
+	public static ResultSet getOracleQueryResult(Connection conn, String tName) {
 		Statement statement = null;
 		ResultSet resultset = null;
-
+		String sql = "select * from " + tName;
 		if (conn != null) {
 			try {
+				// 设置可滚动结果集
 				statement = conn.createStatement(
 						ResultSet.TYPE_SCROLL_SENSITIVE,
 						ResultSet.CONCUR_UPDATABLE);
 				resultset = statement.executeQuery(sql);
 
 			} catch (SQLException e) {
-				e.printStackTrace();
+				System.out.println("查询表出错！");
 			}
 		}
 		return resultset;
 	}
 
-	public static boolean createOracleTable(Connection conn, String tName) {
-		Statement statement = null;
-		try {
-			statement = conn.createStatement();
-			statement.execute("create table " + tName + "("
-					+ "id number(10) primary key not null,"
-					+ "name varchar2(50)," + "sex varchar2(5),"
-					+ "birthday date" + ")");
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-
-	public static boolean insertDataIntoOracleTable(Connection conn,
-			String tName, List<Object[]> lists) {
-		PreparedStatement preStatement = null;
-		try {
-			preStatement = conn.prepareStatement("insert into " + tName
-					+ "(id, name, sex, birthday) Values(?,?,?,?)");
-			conn.setAutoCommit(false);
-			int size = lists.size();
-			Object[] obj = null;
-			for (int i = 0; i < size; i++) {
-				obj = lists.get(i);
-				for (int j = 0; j < obj.length; j++) {
-					preStatement.setObject(j + 1, obj[j]);
-				}
-				preStatement.addBatch();
-			}
-
-			preStatement.executeBatch();
-			conn.commit();
-			conn.setAutoCommit(true);
-		} catch (SQLException e) {
-			e.printStackTrace();
-			try {
-				conn.rollback();
-				conn.setAutoCommit(true);
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-			}
-			return false;
-		}		
-		return true;
-	}
-
+	// 删除表
 	public static boolean dropOracleTable(Connection conn, String tName) {
 		Statement statement = null;
 		try {
 			statement = conn.createStatement();
 			statement.execute("drop table " + tName);
+			System.out.println(tName + "表已删除！");
 		} catch (SQLException e) {
-			e.printStackTrace();
+			System.out.println(tName + "表删除失败！");
+			System.out.println("出错信息：" + e.getLocalizedMessage());
 			return false;
 		}
 		return true;
+	}
+
+	// 打印ResultSet结果集
+	public static void printResultSet(ResultSet resultSet) {
+		if (resultSet == null) {
+			System.out.println("结果集为空");
+			return;
+		}
+		String dno = null;
+		String dname = null;
+
+		try {
+			System.out.println("顺序查：");
+			while (resultSet.next()) {
+				dno = resultSet.getString(1);
+				dname = resultSet.getString(2);
+				System.out.println(dno + ": " + dname);
+			}
+
+			System.out.println("逆序查：");
+			while (resultSet.previous()) {
+				dno = resultSet.getString(1);
+				dname = resultSet.getString(2);
+				System.out.println(dno + ": " + dname);
+			}
+		} catch (SQLException e) {
+			System.out.println("打印ResultSet出错！");
+		}
+	}
+
+	// 关闭连接
+	public static void closeConnection(Connection conn) {
+		try {
+			if (conn == null) {
+				return;
+			}
+			conn.close();
+			System.out.println("数据库连接已关闭");
+		} catch (SQLException e) {
+			System.out.println("关闭数据库连接出错！");
+		}
 	}
 }
